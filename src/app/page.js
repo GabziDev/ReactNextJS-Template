2f48189c1da802cb21bdd7a2ab8a13a024fad585
GabziDev <minecraf2.ggabz190516@gmail.com>
2023-10-10 15:42:24 +0200

    Update Hero component

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
         <Header/>
-        <Hero/>
+        <Hero title="TEMPLATE" subtitle="ReactJS & NextJS"/>
         <Footer/>
     </main>
   )
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Hero.css"
 
-const Hero = () => {
+const Hero = ({title,subtitle}) => {
   return (
     <div className='hero-component'>
-        <h1>Test</h1>
+        <p className='title'>{title}</p>
+        <p className='subtitle'>{subtitle}</p>
     </div>
   )
 }
