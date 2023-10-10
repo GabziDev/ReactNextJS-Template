0dcb879aa1ebbb5433f67410d19eaf39f8a4ca34
GabziDev <minecraf2.ggabz190516@gmail.com>
2023-10-10 15:46:19 +0200

    Update page 'un' & 'deux'

--- src/app/deux/page.js
+++ src/app/deux/page.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 
 const Deux = () => {
     return (
         <>
             <Header/>
-            <div>Page : Deux</div>
+            <Hero title="Page 'DEUX'"/>
             <Footer/>
         </>
     )
--- src/app/un/page.js
+++ src/app/un/page.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Hero from "@/components/Hero";
 
 const Un = () => {
     return (
         <>
             <Header/>
-            <div>Page : Un</div>
+            <Hero title="Page 'UN'"/>
             <Footer/>
         </>
     )
