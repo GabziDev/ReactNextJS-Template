78b560b2ef67e403dac7aea0b4772d97045ac31e
minecraft2.ggabz190516@gmail.com <minecraft2.ggabz190516@gmail.com>
2023-10-10 21:57:45 +0200

    Update footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,13 @@ import React from 'react'
 import "./Footer.css"
 
 const Footer = () => {
+    const dateActuelle = new Date();
+    const anneeActuelle = dateActuelle.getFullYear();
+
     return (
         <div className='footer-component'>
             <footer className="footer">
-                <p className="text">© 2023 Gabzdev. Tous droits réservés.</p>
+                <p className="text">© {anneeActuelle} Gabzdev. Tous droits réservés.</p>
             </footer>
         </div>
     )
